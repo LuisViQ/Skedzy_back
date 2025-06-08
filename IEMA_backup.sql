4d2b987a24fef6f30c2821a5e5be19b4e64d8adc
Luisgato21 <fileps2009@gmail.com>
2025-06-08 14:06:11 -0300

    atualizado

--- IEMA_backup.sql
+++ IEMA_backup.sql
@@ -41,44 +41,8 @@ CREATE TABLE `alocacao` (
   CONSTRAINT `alocacao_ibfk_3` FOREIGN KEY (`id_professor`) REFERENCES `professor` (`id_professor`),
   CONSTRAINT `alocacao_ibfk_4` FOREIGN KEY (`id_sala`) REFERENCES `sala` (`id_sala`),
   CONSTRAINT `alocacao_ibfk_5` FOREIGN KEY (`id_horario`) REFERENCES `horario` (`id_horario`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `alocacao`
---
-
-LOCK TABLES `alocacao` WRITE;
-/*!40000 ALTER TABLE `alocacao` DISABLE KEYS */;
-INSERT INTO `alocacao` VALUES (1,1,1,1,1,1,'Segunda'),(2,1,1,1,1,1,'Terça'),(3,1,1,1,1,1,'Sábado'),(4,1,1,1,2,1,'Sexta'),(5,1,2,1,1,1,'Quinta'),(6,1,2,3,1,1,'Quarta'),(7,1,1,2,1,2,'Segunda');
-/*!40000 ALTER TABLE `alocacao` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `alocacao_professor`
---
-
-DROP TABLE IF EXISTS `alocacao_professor`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `alocacao_professor` (
-  `id_alocacao` int NOT NULL,
-  `id_professor` int NOT NULL,
-  PRIMARY KEY (`id_alocacao`,`id_professor`),
-  KEY `id_professor` (`id_professor`),
-  CONSTRAINT `alocacao_professor_ibfk_1` FOREIGN KEY (`id_alocacao`) REFERENCES `alocacao` (`id_alocacao`),
-  CONSTRAINT `alocacao_professor_ibfk_2` FOREIGN KEY (`id_professor`) REFERENCES `professor` (`id_professor`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `alocacao_professor`
---
-
-LOCK TABLES `alocacao_professor` WRITE;
-/*!40000 ALTER TABLE `alocacao_professor` DISABLE KEYS */;
-/*!40000 ALTER TABLE `alocacao_professor` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `disciplina`
@@ -92,18 +56,8 @@ CREATE TABLE `disciplina` (
   `nome` varchar(100) NOT NULL,
   `carga_horaria` int NOT NULL DEFAULT '40',
   PRIMARY KEY (`id_disciplina`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `disciplina`
---
-
-LOCK TABLES `disciplina` WRITE;
-/*!40000 ALTER TABLE `disciplina` DISABLE KEYS */;
-INSERT INTO `disciplina` VALUES (1,'Matemática',60),(2,'Português',40),(3,'Programação',80),(4,'Banco de Dados',60);
-/*!40000 ALTER TABLE `disciplina` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `horario`
@@ -121,16 +75,6 @@ CREATE TABLE `horario` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `horario`
---
-
-LOCK TABLES `horario` WRITE;
-/*!40000 ALTER TABLE `horario` DISABLE KEYS */;
-INSERT INTO `horario` VALUES (1,'07:35:00','08:25:00'),(2,'08:25:00','09:05:00'),(3,'09:30:00','10:20:00'),(4,'10:20:00','11:10:00'),(5,'11:10:00','12:00:00'),(6,'13:30:00','14:20:00'),(7,'14:20:00','15:00:00'),(8,'15:25:00','16:10:00'),(9,'16:10:00','17:00:00');
-/*!40000 ALTER TABLE `horario` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `professor`
 --
 
@@ -141,18 +85,8 @@ CREATE TABLE `professor` (
   `id_professor` int NOT NULL AUTO_INCREMENT,
   `nome` varchar(100) NOT NULL,
   PRIMARY KEY (`id_professor`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `professor`
---
-
-LOCK TABLES `professor` WRITE;
-/*!40000 ALTER TABLE `professor` DISABLE KEYS */;
-INSERT INTO `professor` VALUES (1,'Prof. João'),(2,'Profª. Maria'),(3,'Prof. Carlos');
-/*!40000 ALTER TABLE `professor` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `sala`
@@ -165,18 +99,8 @@ CREATE TABLE `sala` (
   `id_sala` int NOT NULL AUTO_INCREMENT,
   `nome` varchar(100) NOT NULL,
   PRIMARY KEY (`id_sala`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `sala`
---
-
-LOCK TABLES `sala` WRITE;
-/*!40000 ALTER TABLE `sala` DISABLE KEYS */;
-INSERT INTO `sala` VALUES (1,'Laboratório'),(2,'Sala de Aula');
-/*!40000 ALTER TABLE `sala` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `turma`
@@ -192,16 +116,6 @@ CREATE TABLE `turma` (
   PRIMARY KEY (`id_turma`)
 ) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `turma`
---
-
-LOCK TABLES `turma` WRITE;
-/*!40000 ALTER TABLE `turma` DISABLE KEYS */;
-INSERT INTO `turma` VALUES (1,'101','Técnico em Administração'),(2,'102','Técnico em Farmácia'),(3,'103','Técnico em Informática'),(4,'104','Técnico em Segurança do Trabalho'),(5,'201','Técnico em Administração'),(6,'202','Técnico em Logística'),(7,'203','Técnico em Informática'),(8,'204','Técnico em Segurança do Trabalho'),(9,'301','Técnico em Administração'),(10,'302','Técnico em Serviço Jurídico'),(11,'303','Técnico em Informática'),(12,'304','Técnico em Enfermagem');
-/*!40000 ALTER TABLE `turma` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -212,4 +126,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-05-10 19:52:41
+-- Dump completed on 2025-06-08 14:04:44
